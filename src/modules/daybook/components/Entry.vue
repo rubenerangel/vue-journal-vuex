5888d2849aa8652aa4b655fc54d5741df9977ff2
Ruben Rangel <rubenrang@gmail.com>
2022-08-08 13:25:49 -0500

    Entry Data

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,22 +1,50 @@
 <template>
   <div 
-    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
     class="mb-3 pointer p-2 entry-container"
     >
     <div class="entry-title d-flex">
-        <span class="text-success fs-5 fw-bold">15</span>
-        <span class="mx-1 fs-5">July</span>
-        <span class="mx-2" fw-light>2021, jueves</span>
+        <span class="text-success fs-5 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-5">{{ month }}</span>
+        <span class="mx-2" fw-light>{{ yearDay }}</span>
     </div>
     <div class="entry-descripton">
-        Consequat est dolore occaecat reprehenderit quis consectetur do cupidatat magna velit deserunt. Sunt est nostrud eu ex mollit elit elit id Lorem. Aute ipsum commodo esse aliquip deserunt est magna reprehenderit nisi nisi culpa laboris reprehenderit. Proident pariatur culpa velit esse exercitation deserunt sit enim laboris cupidatat cillum Lorem. Labore culpa anim cillum consectetur cillum aliquip commodo eu do commodo ex esse cillum. Proident mollit Lorem irure enim nostrud quis veniam magna voluptate dolor Lorem enim. Pariatur sint sunt irure ex magna ut consequat sunt aliqua velit elit anim.
+        {{ shortText }}
     </div>
   </div>
 </template>
 
 <script>
-export default {
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
 
+export default {
+    props: {
+        entry: {
+            type: Object,
+            required: true
+        }
+    },
+    computed: {
+        shortText() {
+            return ( this.entry.text.length > 130)
+                ? this.entry.text.substring(0, 130) + '...'
+                : this.entry.text
+        },
+        day() {
+            const date = new Date(this.entry.date)
+            
+            return date.getDate()
+        },
+        month() {
+            const date = new Date(this.entry.date)
+            return months[date.getMonth()]
+        },
+        yearDay() {
+            const date = new Date(this.entry.date)
+            return `${date.getFullYear()}, ${days[date.getDay()]}`
+        }
+    }
 }
 </script>
 
--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -2,14 +2,16 @@
   <div class="entry-list-container">
     <div class="px-2 pt-2">
       <input
+        v-model="term"
         type="text"
         class="form-control" 
         placeholder="Search Entry"/>
     </div>
     <div class="entry-scrollarea">
       <Entry
-        v-for="item in 100"
-        :key="item"
+        v-for="entry in entriesByTerm"
+        :key="entry.id"
+        :entry="entry"
       />
     </div>
   </div>
@@ -17,11 +19,23 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     Entry: defineAsyncComponent(() => import('./Entry.vue'))
   },
+  computed:{
+    ...mapGetters('journal', ['getEntriesByTerm']),
+    entriesByTerm() {
+      return this.getEntriesByTerm( this.term )
+    },
+  },
+  data() {
+    return {
+      term: ''
+    }
+  }
 }
 </script>
 
